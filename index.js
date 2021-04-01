a5fb9e0f6a26d56e8b6332ccd91cb5bb2a7ebaf4
leeseewoo <leeseewoo@gmail.com>
2021-04-01 19:27:36 +0900

    Update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
 const { User } = require("./models/User");
+const cookieParser = require('cookie-parser')
 
 const config = require("./config/key");
 
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser);
 
 const mongoose = require('mongoose')
 mongoose.connect( config.mongoURI, {
@@ -26,8 +28,6 @@ app.get('/', (req, res) => {   res.send('Hello World!!!!!!!!!!!') })
 
 app.post('/register', (req, res) => {
 
-
-
   const user = new User(req.body)
 
   user.save((err, userInfo) => {
@@ -37,8 +37,38 @@ app.post('/register', (req, res) => {
     })
   })
   
+})
 
-  
+app.post('/login', (req, res) => {
+
+  // 요청된 이메일을 DB에서 찾는다
+  User.findOne({ email: req.body.email}, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "User Not Found"
+      })
+    }
+     // 비밀번호가 일치하는지 확인 
+     user.comparePassword(req.body.password, (err, isMatch ) => {
+       if (!isMatch)
+        return res.json ({ loginSuccess: false, message: "password not match"})
+      
+      user.generateToken(( err, user) => {
+        if(err) return res.status(400).send(err);
+
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({ loginSuccess: true, userId: user._id})
+
+      })
+
+
+     })
+
+  })
+ 
+
 })
 
 app.listen(port, () => {   console.log(`Example app listening at http://localhost:${port}`) })
